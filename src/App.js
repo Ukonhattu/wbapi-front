71a0aaed6a12c35c4166c34a41d8d8ea4eec1b51
Daniel Koch <daniel.koch@helsinki.fi>
2019-02-17 15:50:27 +0200

    reat-vis

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios'
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+
+
+const Year = ({year, value}) => {
+  return (
+    <li>{value} : {year[value]}</li>
+  )
 }
 
+const Pop = ({data}) => {
+
+  if (data === [])
+  return (
+    ""
+  )
+  return (
+    JSON.stringify(data)
+  )
+}
+const App = () => {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    console.log('effect')
+    Axios
+    .get('http://localhost:3001/pop/country/Finland/1995/2018')
+    .then(response => {
+      console.log('promise fullfilled')
+      setData(response.data)
+    })
+  }, [])
+  console.log(data)
+  let years = {}
+  if (data.years) years = data
+  return(
+  <div>
+    <h1>Hello World</h1>
+    <Pop data = {data}/>
+  </div>
+)}
 export default App;
