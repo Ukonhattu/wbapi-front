455c1f224fe09462544f7de8caf518a370ea180f
Daniel Koch <daniel.koch@helsinki.fi>
2019-02-17 21:46:20 +0200

    react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,45 +1,95 @@
 import React, { useState, useEffect } from 'react';
 import Axios from 'axios'
 import './App.css';
+import {XYPlot, XAxis, YAxis, VerticalGridLines, HorizontalGridLines, LineSeries, Crosshair} from 'react-vis';
+import '../node_modules/react-vis/dist/style.css'
 
 
 
+const Chart = ({values}) => {
+  const [crosshairValues, setCrosshairValues] = useState([])
+  const dataArr = values.map((p) => {
+    return {x: p.year, y: p.value}
+  })
+  const onMouseLeave = () => {
+    setCrosshairValues([])
+  }
 
-const Year = ({year, value}) => {
+  const onNearestX = (value, index) => {
+    setCrosshairValues([value])
+  }
   return (
-    <li>{value} : {year[value]}</li>
+    <XYPlot
+      onMouseLeave = {onMouseLeave}
+      xType = 'ordinal'
+      width={700}
+      height={400}>
+      <VerticalGridLines />
+      <HorizontalGridLines />
+      <XAxis title = 'Year' />
+      <YAxis title = 'Value'/>
+      <LineSeries
+        onNearestX = {onNearestX}
+        getNull={(p) => p.y !== "" || p.x > 2100}
+        data={dataArr}/>
+      <Crosshair
+        values = {crosshairValues} />
+      </XYPlot>
   )
 }
 
-const Pop = ({data}) => {
+const SearchButton = ({handleChange, search}) => {
 
-  if (data === [])
   return (
-    ""
-  )
-  return (
-    JSON.stringify(data)
+    <form onSubmit={search}>
+      <input 
+        onChange = {handleChange}
+      />
+      <button type = 'submit'>Search</button>
+    </form>
   )
 }
 const App = () => {
   const [data, setData] = useState([])
+  const [query, setQuery] = useState('')
+  const [url, setUrl] = useState('http://localhost:3001/pop/country/Finland/1995/2017')
 
-  useEffect(() => {
-    console.log('effect')
-    Axios
-    .get('http://localhost:3001/pop/country/Finland/1995/2018')
-    .then(response => {
-      console.log('promise fullfilled')
-      setData(response.data)
-    })
-  }, [])
-  console.log(data)
-  let years = {}
-  if (data.years) years = data
+  const search = (event) => {
+    event.preventDefault()
+    setUrl('http://localhost:3001/co2/country/' + query +'/1995/2017')
+    console.log(url)
+  }
+
+  const handleChange = (event) => {
+    setQuery(event.target.value)
+  }
+
+  const getData = () => {
+    console.log('test')
+    useEffect(() => {
+      Axios
+      .get(url)
+      .then(response => {
+        setData(response.data)
+        console.log('effect')
+      })
+    }, [url])
+  }
+  getData()
+  let values = []
+  if (data.country){ 
+    values = data['values']
+  }
+  console.log('values', values)
+  console.log('data', data)
   return(
   <div>
     <h1>Hello World</h1>
-    <Pop data = {data}/>
+    <SearchButton 
+      handleChange = {handleChange}
+      search = {search}
+      />
+    <Chart values = {values}/>
   </div>
 )}
 export default App;
